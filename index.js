277a0f81a5bdf1856da1577d5971ca70ffa97524
1matheusmendes <matheus_mendess@live.com>
2018-09-11 21:54:47 -0300

    animações com js puro

---
+++ index.js
@@ -0,0 +1,11 @@
+//logica
+// 1 - selecionar elementos que devem ser animados
+// 2 - definir a classe que é adicionada durante a animaçãoptimize
+// 3 - criar função de animaçãoptimize
+// 3.1 - verificar a distancia entre a barra de scroll e o topo do sistemas
+// 3.2 - verificar se a distancia 3.1 + offset é maior do que a distancia entre o elemento e o topo da paginas
+// 3.3 - se verdadeiro adicionar classe de animação, remover se for falso
+// 4 - ativar a função de animação toda vez que o usuário utilizar o scroll
+// 5 - otimizar ativação
+
+   
